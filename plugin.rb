081324bb6cf44e74694004e96c16e57acd8ec0e8
Steven Litras <steve@cribl.io>
2021-12-29 12:19:41 -0800

    initial commit

---
+++ plugin.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# name: cribl-leaderboard
+# about: TODO
+# version: 0.0.1
+# authors: Cribl, Inc.
+# url: TODO
+# required_version: 2.7.0
+# transpile_js: true
+
+#enabled_site_setting :plugin_name_enabled
+
+#after_initialize do
+#end
